08f860ca39a93aac323de7a084079efb7dfc7a79
Andre Charles Kemp <59055526+AndreCharlesKemp@users.noreply.github.com>
2020-01-09 22:26:46 +0200

    Task 14

---
+++ Task 14/MLearning.py
@@ -0,0 +1,46 @@
+#Supervised Learning:
+#In supervised learning problems, a program predicts an                        
+#output for an input by learning from pairs of labeled inputs and outputs; that is, the                                
+#program learns from examples of the right answers.
+#Models of supervised learning include:
+    #Support vector machiens(SVMs)
+    #Kernel functions
+    #Naïve Bayes classifiers
+    #Adaptive boosting(AdaBoost)
+
+#Unsupervised Learning:
+#a progra does not learn from labeled data. Instead, it attempts to discover
+#patterns in the data.
+#Models of unsupervised learning include:
+    #Clustering(K-means Clustering, Heirarchical Clustering, Probabilistic Clustering)
+    #Data compresison
+    #Deep learning
+
+#Semi-supervised Learning:
+#the machine will attempt to devise an algorithm on its own.
+#however, the dataset to pull teh data from will need to be "labeled"
+#in which each record includes the outcome information.
+#Models of seim-supervised learning include:
+    #Latent-feature discriminative model (M1)
+    #Generative semi-supervised model (M2)
+    #Stacked generative semi-supervised model (M1+M2)
+    #Source: https://pdfs.semanticscholar.org/b6b9/39ffc9920cd8521299a6fe9ec55775f9bf3c.pdf
+
+
+
+
+#Machine learning is a method of data analysis that automates
+#analytical model building. It is a branch of artificial intelligence
+#based on the idea that systems can learn from data, identify patterns
+#and make decisions with minimal human intervention.
+#The implementations of machine learning are nearly limitless.
+#It can be used for something as advanced as the self-driving car,that will
+#adapt to its surroundings using cameras and/or distance determination based on sound.
+#Furthermore, it can be used to monitor the use of floors or rooms in a large building,
+#allowing information such as frequency of people using bathrooms on one level compared
+#to another bathroom, which will require less stocking of paper towels, or refills of soap.
+#The upside is that there will be far fewer mistakes made in operational decision-making.
+#However, right now, with our current understanding, machine learning will be completely
+#amoral, making its use in military applications very dangerous, depending on how it
+#learns who is a threat. It can lead to preemptive attacks for the safety of those it
+#considers to be the people its protecting.
